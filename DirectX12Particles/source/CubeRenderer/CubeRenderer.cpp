9df4bee643f2662fa0e770b0c039bff4546b8ee3
lukephilipps <85194228+lukephilipps@users.noreply.github.com>
2024-06-13 15:59:28 -0700

    CubeRenderer header file

--- DirectX12Particles/source/CubeRenderer/CubeRenderer.cpp
+++ DirectX12Particles/source/CubeRenderer/CubeRenderer.cpp
@@ -1,2 +1,8 @@
 #include "pch.h"
 #include "CubeRenderer.h"
+
+#include "Application.h"
+#include "CommandQueue.h"
+#include "Window.h"
+
+using namespace DirectX;
--- DirectX12Particles/source/CubeRenderer/CubeRenderer.h
+++ DirectX12Particles/source/CubeRenderer/CubeRenderer.h
@@ -7,5 +7,62 @@
 
 class CubeRenderer : Game
 {
+public:
 
+	using super = Game;
+
+	CubeRenderer(const std::wstring& name, int width, int height, bool vSync = false);
+	virtual bool LoadContent() override;
+	virtual void UnloadContent() override;
+
+protected:
+
+	virtual void OnUpdate(UpdateEventArgs& e) override;
+	virtual void OnRender(RenderEventArgs& e) override;
+	virtual void OnKeyPressed(KeyEventArgs& e) override;
+	virtual void OnMouseWheel(MouseWheelEventArgs& e) override;
+	virtual void OnResize(ResizeEventArgs& e) override;
+
+private:
+
+	// Transition a resource's state
+	void TransitionResource(ComPtr<ID3D12GraphicsCommandList2> commandList, ComPtr<ID3D12Resource> resource, D3D12_RESOURCE_STATES beforeState, D3D12_RESOURCE_STATES afterState);
+
+	// Clear the color of a render-target-view
+	void ClearRTV(ComPtr<ID3D12GraphicsCommandList2> commandList, D3D12_CPU_DESCRIPTOR_HANDLE rtv, FLOAT* clearColor);
+
+	// Clear the depth of a depth-stencil-view
+	void ClearDepth(ComPtr<ID3D12GraphicsCommandList2> commandList, D3D12_CPU_DESCRIPTOR_HANDLE dsv, FLOAT depth = 1.0f);
+
+	// Create a GPU buffer
+	void UpdateBufferResource(ComPtr<ID3D12GraphicsCommandList2> commandList, ID3D12Resource** pDestinationResource, ID3D12Resource** pIntermediateResource, 
+		size_t numElements, size_t elementSize, const void* bufferData, D3D12_RESOURCE_FLAGS flags = D3D12_RESOURCE_FLAG_NONE);
+
+	// Resize the depth buffer to match the window area
+	void ResizeDepthBuffer(int width, int height);
+
+	uint64_t FenceValues[Window::BufferCount] = {};
+
+	ComPtr<ID3D12Resource> VertexBuffer;
+	D3D12_VERTEX_BUFFER_VIEW VertexBufferView;
+
+	ComPtr<ID3D12Resource> IndexBuffer;
+	D3D12_INDEX_BUFFER_VIEW IndexBufferView;
+
+	ComPtr<ID3D12Resource> DepthBuffer;
+	ComPtr<ID3D12DescriptorHeap> DSVHeap;
+
+	ComPtr<ID3D12RootSignature> RootSignature;
+	ComPtr<ID3D12PipelineState> PipelineState;
+
+	D3D12_VIEWPORT Viewport;
+	D3D12_RECT ScissorRect;
+
+	float FoV;
+
+	DirectX::XMMATRIX ModelMatrix;
+	DirectX::XMMATRIX ViewMatrix;
+	DirectX::XMMATRIX ProjectionMatrix;
+
+	bool ContentLoaded;
 };
