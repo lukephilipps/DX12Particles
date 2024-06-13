e1680647fcb54b9ca95f413aa3f9eee59b69d819
lukephilipps <85194228+lukephilipps@users.noreply.github.com>
2024-06-13 16:22:33 -0700

    Update d3dx12.h file

--- DirectX12Particles/source/CubeRenderer/CubeRenderer.cpp
+++ DirectX12Particles/source/CubeRenderer/CubeRenderer.cpp
@@ -5,4 +5,41 @@
 #include "CommandQueue.h"
 #include "Window.h"
 
+#include "d3dx12.h"
+
 using namespace DirectX;
+
+struct VertexPosColor
+{
+	XMFLOAT3 Position;
+	XMFLOAT3 Color;
+};
+
+static VertexPosColor Vertices[8] = {
+	{ XMFLOAT3(-1.0f, -1.0f, -1.0f), XMFLOAT3(0.0f, 0.0f, 0.0f) },
+	{ XMFLOAT3(-1.0f,  1.0f, -1.0f), XMFLOAT3(0.0f, 1.0f, 0.0f) },
+	{ XMFLOAT3( 1.0f,  1.0f, -1.0f), XMFLOAT3(1.0f, 1.0f, 0.0f) },
+	{ XMFLOAT3( 1.0f, -1.0f, -1.0f), XMFLOAT3(1.0f, 0.0f, 0.0f) },
+	{ XMFLOAT3(-1.0f, -1.0f,  1.0f), XMFLOAT3(0.0f, 0.0f, 1.0f) },
+	{ XMFLOAT3(-1.0f,  1.0f,  1.0f), XMFLOAT3(0.0f, 1.0f, 1.0f) },
+	{ XMFLOAT3( 1.0f,  1.0f,  1.0f), XMFLOAT3(1.0f, 1.0f, 1.0f) },
+	{ XMFLOAT3( 1.0f, -1.0f,  1.0f), XMFLOAT3(1.0f, 0.0f, 1.0f) }
+};
+
+static WORD Indices[36] = {
+	0, 1, 2, 0, 2, 3,
+	4, 6, 5, 4, 7, 6,
+	4, 5, 1, 4, 1, 0,
+	3, 2, 6, 3, 6, 7,
+	1, 5, 6, 1, 6, 2,
+	4, 0, 3, 4, 3, 7
+};
+
+CubeRenderer::CubeRenderer(const std::wstring& name, int width, int height, bool vSync)
+	: super(name, width, height, vSync)
+	, ScissorRect(CD3DX12_RECT(0, 0, LONG_MAX, LONG_MAX))
+	, Viewport(CD3DX12_VIEWPORT(0.0f, 0.0f, static_cast<float>(width), static_cast<float>(height)))
+	, FoV(45.0)
+	, ContentLoaded(false)
+{
+}
