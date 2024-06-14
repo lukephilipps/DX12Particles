692ffe53bdbc09f1e57c9aedc635620a6a01faf8
lukephilipps <85194228+lukephilipps@users.noreply.github.com>
2024-06-14 02:35:40 -0700

    Update CubeRenderer.cpp

--- DirectX12Particles/source/CubeRenderer/CubeRenderer.cpp
+++ DirectX12Particles/source/CubeRenderer/CubeRenderer.cpp
@@ -44,8 +44,66 @@ CubeRenderer::CubeRenderer(const std::wstring& name, int width, int height, bool
 {
 }
 
+void CubeRenderer::UpdateBufferResource(ComPtr<ID3D12GraphicsCommandList2> commandList, ID3D12Resource** pDestinationResource, ID3D12Resource** pIntermediateResource,
+	size_t numElements, size_t elementSize, const void* bufferData, D3D12_RESOURCE_FLAGS flags)
+{
+	auto device = Application::Get().GetDevice();
+
+	size_t bufferSize = numElements * elementSize;
+
+	CD3DX12_RESOURCE_DESC vertexBufferDesc = CD3DX12_RESOURCE_DESC::Buffer(bufferSize, flags);
+
+	// Create a committed resource in GPU mem
+	CD3DX12_HEAP_PROPERTIES defaultHeapProperties(D3D12_HEAP_TYPE_DEFAULT);
+	ThrowIfFailed(device->CreateCommittedResource(
+		&defaultHeapProperties,
+		D3D12_HEAP_FLAG_NONE,
+		&vertexBufferDesc,
+		D3D12_RESOURCE_STATE_COMMON,
+		nullptr,
+		IID_PPV_ARGS(pDestinationResource)));
+
+	// Create a committed resource for the upload
+	if (bufferData)
+	{
+		CD3DX12_HEAP_PROPERTIES uploadHeapProperties(D3D12_HEAP_TYPE_UPLOAD);
+		ThrowIfFailed(device->CreateCommittedResource(
+			&uploadHeapProperties,
+			D3D12_HEAP_FLAG_NONE,
+			&vertexBufferDesc,
+			D3D12_RESOURCE_STATE_GENERIC_READ,
+			nullptr,
+			IID_PPV_ARGS(pIntermediateResource)));
+
+		D3D12_SUBRESOURCE_DATA subresourceData = {};
+		subresourceData.pData = bufferData;
+		subresourceData.RowPitch = bufferSize;
+		subresourceData.SlicePitch = subresourceData.RowPitch;
+
+		UpdateSubresources(commandList.Get(), *pDestinationResource, *pIntermediateResource, 0, 0, 1, &subresourceData);
+	}
+}
+
 bool CubeRenderer::LoadContent()
 {
+	auto device = Application::Get().GetDevice();
+	auto commandQueue = Application::Get().GetCommandQueue(D3D12_COMMAND_LIST_TYPE_COPY);
+	auto commandList = commandQueue->GetCommandList();
+
+	ComPtr<ID3D12Resource> intermediateVertexBuffer;
+	UpdateBufferResource(commandList.Get(), &VertexBuffer, &intermediateVertexBuffer, _countof(Vertices), sizeof(VertexPosColor), Vertices);
+
+	VertexBufferView.BufferLocation = VertexBuffer->GetGPUVirtualAddress();
+	VertexBufferView.SizeInBytes = sizeof(Vertices);
+	VertexBufferView.StrideInBytes = sizeof(VertexPosColor);
+
+	ComPtr<ID3D12Resource> intermediateIndexBuffer;
+	UpdateBufferResource(commandList.Get(), &IndexBuffer, &intermediateIndexBuffer, _countof(Indices), sizeof(WORD), Indices);
+
+	IndexBufferView.BufferLocation = IndexBuffer->GetGPUVirtualAddress();
+	IndexBufferView.Format = DXGI_FORMAT_R16_UINT;
+	IndexBufferView.SizeInBytes = sizeof(Indices);
+
 	return true;
 }
 
